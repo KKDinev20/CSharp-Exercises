da5318c68f0b34a7880fe7e1b36250281b87c9dd
KKDinev20 <KKDinev20@codingburgas.bg>
2024-03-20 23:15:23 +0200

    Add missing files

---
+++ CSharpEx/ActionPrint/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> names = new List<string>();
+
+        Console.WriteLine("Enter names (press Enter on an empty line to finish):");
+        string input;
+        while (!string.IsNullOrWhiteSpace(input = Console.ReadLine()))
+        {
+            names.Add(input);
+        }
+
+        Action<string> printName = name => Console.WriteLine(name);
+
+        Console.WriteLine("Names entered:");
+        names.ForEach(printName);
+    }
+}
